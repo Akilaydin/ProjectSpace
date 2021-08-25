07ac820fc43021cc2e2325d8dce77a1462482fe5
Eren Black <84272943+VineSsmoke@users.noreply.github.com>
2021-08-25 18:17:44 +0300

    cameraMover

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector2 movingDirection;
+
+    private Transform selfTransform;
+    
+    
+    void Start()
+    {
+        selfTransform = transform;
+    }
+
+    
+    void Update()
+    {
+        Move(); 
+    }
+
+    private void Move()
+    {
+        selfTransform.Translate(movingDirection * speed * Time.deltaTime);
+    }
+}
