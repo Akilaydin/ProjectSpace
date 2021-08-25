dc29c098c4168214a454aca12661a0dfefbe899b
Eren Black <84272943+VineSsmoke@users.noreply.github.com>
2021-08-25 21:23:55 +0300

    Скрипт Управления Кораблем

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float distance;
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+
+}
