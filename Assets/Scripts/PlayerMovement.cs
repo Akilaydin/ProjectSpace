b47b29664497d569ef4549d1add7c3b8ec03ed3c
Artem Ovchinnikov <artem.o@amayasoft.uz>
2021-08-28 08:37:05 +0300

    Player movement changed. Added DOTween

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float distance;
+    private float _distance;
+
+    [SerializeField]
+    private float _duration;
 
     private void OnMouseDrag()
     {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = objPosition;
-    }
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
+        transform.DOMove(new Vector3(objPosition.x, objPosition.y + _distance), _duration, false);
+    }
 }
