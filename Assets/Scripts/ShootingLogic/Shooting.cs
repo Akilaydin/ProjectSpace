f6f8d46d1041c44ccb03bdeb71f659d9b28629a4
Eren Black <84272943+VineSsmoke@users.noreply.github.com>
2021-09-01 18:14:45 +0300

    coolDown

--- Assets/Scripts/ShootingLogic/Shooting.cs
+++ Assets/Scripts/ShootingLogic/Shooting.cs
@@ -7,9 +7,29 @@ public class Shooting : MonoBehaviour
     [SerializeField]
     private AddressableObjectPooler _pooler;
 
-    private void LateUpdate()
+    [SerializeField]
+    private float _fireSpeed;
+
+    private float _coolDownTime;
+
+    private bool _hasShot;
+
+    private void Update()
     {
-        Shoot();
+        if(_hasShot==true)
+        {
+            _coolDownTime += Time.deltaTime;
+            if(_coolDownTime>=_fireSpeed)
+            {
+                _hasShot = false;
+                _coolDownTime = 0;
+            }
+        }
+        if(_hasShot==false)
+        {
+            Shoot();
+        }
+        
     }
 
     private void Shoot()
@@ -19,5 +39,7 @@ public class Shooting : MonoBehaviour
         {
             return;
         }
+        _hasShot = true;
     }
 }
+
