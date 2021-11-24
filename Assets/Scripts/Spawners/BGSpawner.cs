88c5f74363fdedcaa2c0c470a34ce9b31c872cdf
Eren Black <84272943+VineSsmoke@users.noreply.github.com>
2021-11-24 19:26:37 +0300

    Spawn
    aboba

---
+++ Assets/Scripts/Spawners/BGSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _backGround;
+
+    public void Spawn()
+    {
+        
+    }
+
+}
+
--- Assets/Scripts/Utils/Dispatchers/OnTriggerEnterDispatcher.cs
+++ Assets/Scripts/Utils/Dispatchers/OnTriggerEnterDispatcher.cs
@@ -4,7 +4,7 @@ using UnityEngine.Events;
 public class OnTriggerEnterDispatcher : MonoBehaviour
 {
 	[SerializeField]
-	private UnityEvent<GameObject> _entered;
+	private UnityGameObjectEvent _entered;
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
